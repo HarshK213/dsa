e4033262b83bbe78556a148cd3370825b5cdb7ed
HarshK213 <hkushwaha21305@gmail.com>
2025-05-13 15:01:43 +0530

    update rev num for negitive num

--- 12_Maths/RevNum.cpp
+++ 12_Maths/RevNum.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <climits>
+#include <math.h>
 using namespace std;
 
-int revNum(int n){
+int revNum(int x){
+	int n = x;
 	int ans = 0;
-	while(n>0){
+	while(abs(n)>0){
 		int lastDig = n%10;
 		n=n/10;
 		if(ans > INT_MAX/10 || ans < INT_MIN/10)return 0;
 		ans = (ans*10) + lastDig;
 	}
+	if(x<0)return (ans);
 	return ans;
 }
 
 int main(){
-	cout << revNum(1234) << endl;
+	cout << revNum(-1234) << endl;
 	return 0;
 }
